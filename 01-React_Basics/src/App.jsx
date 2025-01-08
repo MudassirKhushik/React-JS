f5d22f5b5afecce2116c1d8da323224cbb1d5da6
Mudassir Khushik <mudassirkhushik@gmail.com>
2025-01-08 08:27:53 -0800

    File Structure and Creating our React Element

--- 01-React_Basics/src/App.jsx
+++ 01-React_Basics/src/App.jsx
@@ -6,9 +6,10 @@ function App() {
     // You can only export one element, either wrap it in a div or just return one element.
     // Because of this here comes the concept of fragment (<> </>).
     <>
+    const username = "Mudassir Khushik" // This will be treated as a variable with curly braces.
     <New_fun/>
     <div>This is a div element</div>
-    <h1>This is a h1 tag element</h1>
+    <h1>This is a h1 tag element {username}</h1>
     <p>Here is a paragraph</p>
     </>
   )
--- 01-React_Basics/src/main.jsx
+++ 01-React_Basics/src/main.jsx
@@ -1,9 +1,56 @@
-import { StrictMode } from 'react'
+import React, { Children, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1> Custom App</h1>
+    </div>
+  )
+}
+
+// This code is converted into the code given below. 
+// We write that directly to check if we save one step of the bundler
+
+// const reactElement = {
+//   type : 'a',
+//   props : {
+//     href : 'https://google.com',
+//     target : '_blank',
+//   },
+//   Children : 'Click me to visit Google'
+// }
+
+
+const anotherUser = " Mudassir Khushik"
+
+// This is used in another way
+// This React contains the createElement method and has special Syntax
+const reactElement = React.createElement(
+  'a',
+  {
+    href : 'https://google.com',
+    target : '_blank',
+  },
+  'Click me to visit Google',
+  anotherUser
+)
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank"> Visit Google</a>
+)
+ 
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+//    {/* <reactElement/> This is an object and an object is not called like a function {Neither reactElement(), nor reactElement} */}
+
+    //anotherElement // This will work 
+//    {/* This will work because we are using correct syntax and reactElement doesn't have the correct syntax */}
+
+//    <App />
+
+// After creating the reactElement in a proper syntax we can call it
+  reactElement
 )
